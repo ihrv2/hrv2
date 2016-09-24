f6d5ab063af4b582f99557663667606cdf91246c
azrizal <azrizal@msd.net.my>
2016-09-25 00:43:34 +0800

    update 25/9/2016 12:43am

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'ihrv2.igw@gmail.com', 'name' => 'IHR Admin'],
 
     /*
     |--------------------------------------------------------------------------
